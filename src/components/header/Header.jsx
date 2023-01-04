c74821d01a1379b85e1b3686cbd159ecbac99bd7
Ahmed Al Beruny <ahmedberuny@hotmail.com>
2023-01-04 19:09:09 +0600

    commit2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,19 +4,31 @@ import './header.css'
 export default function Header() {
   return (
     <div className='header-container'>
-      
-      <div className="nav-bar">
+
+      <div className='nav-bar'>
+
+      <div className="logo">
+        BERUNY
+      </div>
+
+      <div className="nav-bar-center">
         <div className="nav-item">Home</div>
+        <div className="nav-item">About</div>
         <div className="nav-item">Skills</div>
         <div className="nav-item">Services</div>
-        <div className="nav-item">About</div>
         <div className="nav-item">Contacts</div>
       </div>
 
+      <div className="toggle">
+        <div className='line'></div>
+        <div className='line'></div>
+        <div className='line'></div>
+      </div>
+
+    </div>
 
 
-
-      <h1>Lorem ipsum, dolor sit amet consectetur adipisicing lore.</h1>
+      <h4 className='head-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium illum quisquam, eligendi vel repellat eveniet.</h4>
     </div>
   )
 }
