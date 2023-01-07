f747c8f111cb322355c2ecb2f8248591cd42c033
Ahmed Al Beruny <ahmedberuny@hotmail.com>
2023-01-07 09:23:22 +0600

    j

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from './components/header/Header';
 import Skills from './components/skills/Skills';
-import Services from './components/services/Services';
 import About from './components/about/About';
 import Contacts from './components/contacts/Contacts';
 import Footer from './components/footer/Footer';
