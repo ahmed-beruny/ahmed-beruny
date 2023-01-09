7eb1979ef9b1ea141566805512438420d1935eaa
ahmed-beruny <beruny10@gmail.com>
2023-01-09 14:00:02 +0600

    a

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import "./footer.css"
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+
+      <h2>developed in React JS</h2>
+
+    </div>
   )
 }
