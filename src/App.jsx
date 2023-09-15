016634aff8288923f9ed6ec406d3b5be4d596eb5
Ahmed Al Beruny <ahmedberuny@hotmail.com>
2023-09-15 15:20:45 +0600

    issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" exact element={<Main />} />
         <Route path="/blogs" element={<Blogs />} />
       </Routes>
     </Router>
