9a021645f6aa2380111cfb45e7a653eff2c45ca4
Ahmed Al Beruny <ahmedberuny@hotmail.com>
2023-09-21 18:17:24 +0600

    fwerwewsf

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,23 @@ import './App.css';
 import Blogs from './components/blogs/Blogs';
 
 import Main from './components/main/Main';
+import ReactGa from 'react-ga';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+ReactGa.initialize('UA-182379039-1');
+ReactGa.set({ page: window.location.pathname });
+ReactGa.pageview(window.location.pathname + window.location.search);
+
+
+
 function App() {
+
+  ReactGa.event({
+    category: 'User',
+    action: 'User opened the website'
+  });
+
   return (
     <Router>
       <Routes>
